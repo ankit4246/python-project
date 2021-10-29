7477afa37180be52572fb5b002a59b9312ed362a
Aayush Man Singh <mansinghaayush@gmail.com>
2021-10-29 14:56:40 +0545

    minor changes

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,23 @@
 from django.forms import ModelForm
 
-from users.models import Profile
+from users.models import Profile, TrainingDetails, ExperienceDetails, SocialMedias
 
 
 class BasicInformation(ModelForm):
     class Meta:
         model = Profile
+
+
+class TrainingForm(ModelForm):
+    class Meta:
+        model = TrainingDetails
+
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = ExperienceDetails
+
+
+class SocialMediaForm(ModelForm):
+    class Meta:
+        model = SocialMedias
