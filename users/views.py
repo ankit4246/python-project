5e913f18897ca666c7c596b6a85033e985a73327
iamdarshan7 <darshanthapa872@gmail.com>
2021-10-29 13:23:54 +0545

    form and view created for personal,address,education model

--- users/views.py
+++ users/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from users.forms import BasicInformation, AddressInfo, EducationInfo
+from django.urls import reverse_lazy
 
-from users.models import Profile
-
-
-class PersonalInfoView(TemplateView):
+class PersonalInfoView(FormView):
     template_name = 'users/personal_info.html'
+    form_class = BasicInformation
+    success_url = reverse_lazy('address_info')
 
-
-class AddressInfoView(TemplateView):
+class AddressInfoView(FormView):
     template_name = 'users/address_info.html'
+    form_class = AddressInfo
+    success_url = reverse_lazy('education_info')
 
-
-class EducationInfoView(TemplateView):
+class EducationInfoView(FormView):
     template_name = 'users/education_info.html'
-
+    form_class = EducationInfo
+    success_url = reverse_lazy('training_info')
 
 class TrainingInfoView(TemplateView):
     template_name = 'users/training_info.html'
