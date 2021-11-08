dba2c41d58e27952241b95c9150698e092ed6ab2
Aayush Man Singh <mansinghaayush@gmail.com>
2021-11-08 10:13:45 +0545

    minor updates

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('personal_info/', views.PersonalInfoView.as_view(), name='personal_info'),
-    path('address_info/', views.AddressInfoView.as_view(), name='address_info'),
-    path('education_info/', views.EducationInfoView.as_view(), name='education_info'),
-    path('training_info/', views.TrainingInfoView.as_view(), name='training_info'),
-    path('work_info/', views.WorkInfoView.as_view(), name='work_info'),
-    path('social_info/', views.SocialInfoView.as_view(), name='social_info'),
+    path('personal_info/', login_required(views.PersonalInfoView.as_view()), name='personal_info'),
+    path('address_info/', login_required(views.AddressInfoView.as_view()), name='address_info'),
+    path('education_info/', login_required(views.EducationInfoView.as_view()), name='education_info'),
+    path('training_info/', login_required(views.TrainingInfoView.as_view()), name='training_info'),
+    path('work_info/', login_required(views.WorkInfoView.as_view()), name='work_info'),
+    path('social_info/', login_required(views.SocialInfoView.as_view()), name='social_info'),
     # path('register/', views.RegistrationView.as_view(), name='register'),
     path('register/', views.user_register, name='register'),
     path('login/', views.login_user, name="login"),
