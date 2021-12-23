6ae6369844ce1bae97b3ca1b53a59a629d665839
Aayush Man Singh <mansinghaayush@gmail.com>
2021-12-23 13:14:09 +0545

    working on review and submit template integration

--- project/urls.py
+++ project/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('project-details/<int:project_id>/', login_required(views.ProjectDetailsView.as_view()), name='update_project_details'),
     path('project-delete/<int:pk>/', login_required(views.ProjectDeleteView.as_view()), name='delete_project'),
     path('project-target-delete/<int:pk>/', login_required(views.ProjectTargetDeleteView.as_view()), name='delete_project_target'),
-
     path('project-targets/<int:project_id>/', login_required(views.ProjectTargetsView.as_view()), name='create_project_targets'),
+    path('review-and-submit/<int:project_id>/', login_required(views.ReviewSubmitView.as_view()),name='review_and_submit'),
+
     path('project-user/', views.create_project_users_view, name='create_project_user'),
 
     # path('retrieve_project/<int:pk>/', login_required(views.ListProjectsView.as_view()), name='create_project'),
